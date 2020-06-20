d75794c3b973404267294a1e73b36b7d0e497297
Benjamin Fallar III <bfallar3@hotmail.com>
2020-06-20 17:21:59 +0800

    Add Data Validation to the Form

--- BugTrackerUI/Bug.cs
+++ BugTrackerUI/Bug.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,10 +11,15 @@ namespace BugTrackerUI
     {
         public int Id { get; set; }
 
+        [Required]
         public string Title { get; set; }
 
+        [Required]
+        [MinLength(10)]
         public string Description { get; set; }
 
+        [Required]
+        [Range(1,5)]
         public int Priority { get; set; }
     }
 }
